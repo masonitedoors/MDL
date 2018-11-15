8cfb182765cbf1c98b54e4e9bf0cc15abccbc887
Tarik Hamilton <thamilton@masonite.com>
2018-11-15 12:41:17 -0500

    Update Checkbox

--- components/internal/forms/Checkbox/react.js
+++ components/internal/forms/Checkbox/react.js
@@ -3,10 +3,16 @@ import s from "./style.module.scss";
 import svg from "lib/micons/MIcons.svg";
 
 const Checkbox = props => {
+  const { checked, onChange } = props;
   const useTag = `<use xlink:href="${svg}#check" />`;
   return (
-    <div className={s.Checkbox}>
-      <input className={s.Checkbox__checkbox} type="checkbox" />
+    <div className={s.Checkbox} onClick={onChange}>
+      <input
+        className={s.Checkbox__checkbox}
+        type="checkbox"
+        checked={checked}
+        readOnly
+      />
       <div className={s["Checkbox__svg-wrapper"]}>
         <svg
           className={[
--- components/internal/forms/Checkbox/stories.react.js
+++ components/internal/forms/Checkbox/stories.react.js
@@ -1,6 +1,24 @@
 import React from "react";
 import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
+import { withKnobs, boolean } from "@storybook/addon-knobs";
+import { withState } from "@dump247/storybook-state";
 import Checkbox from "./react";
 
-storiesOf("Checkbox", module).add("default", () => <Checkbox />);
+storiesOf("Checkbox", module)
+  .addDecorator(withKnobs)
+  .add("checked from parent state", () => {
+    const checked = boolean("Checked", true);
+    return <Checkbox checked={checked} />;
+  })
+  .add(
+    "checked by its click event",
+    withState({ checked: false })(({ store }) => {
+      console.log("store", store);
+      return (
+        <Checkbox
+          checked={store.state.checked}
+          onChange={() => store.set({ checked: !store.state.checked })}
+        />
+      );
+    })
+  );

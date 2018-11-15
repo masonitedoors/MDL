a89ee994ba84f65b16ed68c52bb3b71631c1051b
Tarik Hamilton <thamilton@masonite.com>
2018-11-14 21:33:01 -0500

    Working build process for packages

---
+++ components/internal/forms/Checkbox/stories.react.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { action } from "@storybook/addon-actions";
+import Checkbox from "./react";
+
+storiesOf("Checkbox", module).add("default", () => <Checkbox />);

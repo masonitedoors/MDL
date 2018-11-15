9e0bf530ff961114f98fe7e4a32a1f0a7add131e
Tarik Hamilton <thamilton@masonite.com>
2018-11-14 21:33:12 -0500

    Move existing components to internal folder

--- components/forms/Checkbox/stories.html.js
+++
@@ -1,5 +0,0 @@
-import { storiesOf } from "@storybook/html";
-import { action } from "@storybook/addon-actions";
-import html from "./html.html";
-
-storiesOf("Checkbox", module).add("default", () => html);

328e51d8dd4d6bcebce9ac424248c005f7abc3e9
Tarik Hamilton <thamilton@masonite.com>
2018-11-16 12:52:01 -0500

    Update exports

--- components/internal/forms/Checkbox/index.js
+++ components/internal/forms/Checkbox/index.js
@@ -1,2 +1,1 @@
 export { default as React } from "./react";
-// export { default as Vue } from "./vue";
---
+++ packages/external-ui-react/src/index.js
@@ -0,0 +1,2 @@
+export { React as Checkbox } from "components/internal/forms/Checkbox";
+export { React as Button } from "components/internal/forms/button/Button";

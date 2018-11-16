3fdd0aaebe366bcfba9dd3da3abece0bbb4ba001
Tarik Hamilton <me@tarikhamilton.com>
2018-11-16 11:12:37 -0500

    Simplify/update packages

--- packages/internal-ui-react/webpack.config.js
+++ packages/internal-ui-react/webpack.config.js
@@ -9,8 +9,5 @@ webpackConfig.output.path = path.resolve(__dirname, "dist");
 webpackConfig.module.rules.push(
   reactRule(path.resolve(__dirname, "./../../components"))
 );
-// webpackConfig.module.rules.push(
-//   svgRule(path.resolve(__dirname, "./../../lib"))
-// );
 
 module.exports = webpackConfig;

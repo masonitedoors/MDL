96ff0723534f10946d6e7c1807fd15b234a30ad6
Tarik Hamilton <thamilton@masonite.com>
2018-11-19 14:20:51 -0500

    Add Tabs component

---
+++ components/internal/nav/Tabs/react.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import s from "./style.module.scss";
+
+export const Tab = ({ active, children, id, onTabClick = () => false }) => {
+  const className = [s.tab, active && s["tab--active"]]
+    .filter(v => v)
+    .join(" ");
+
+  return (
+    <li onClick={() => onTabClick(id)} className={className}>
+      {children}
+    </li>
+  );
+};
+
+export const Tabs = props => {
+  const { activeTab, onTabClick } = props;
+  const tabs = () =>
+    props.tabs.map(({ id, children }) => (
+      <Tab active={id === activeTab} key={id} id={id} onTabClick={onTabClick}>
+        {children}
+      </Tab>
+    ));
+
+  return <ul className={s.tabs}>{tabs()}</ul>;
+};
+
+export default Tabs;

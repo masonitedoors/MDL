7a6716ad58c64662e4de53dd3545d5824c7a8682
Tarik Hamilton <thamilton@masonite.com>
2018-12-31 12:29:43 -0500

    Update Button and Tabs

--- components/internal/forms/button/Button/react.js
+++ components/internal/forms/button/Button/react.js
@@ -5,12 +5,19 @@ import classNames from "classnames/bind";
 const cx = classNames.bind(s);
 
 const Button = props => {
-  const { children, onClick = null, variant = null, uppercase = false } = props;
+  const {
+    children,
+    onClick = null,
+    variant = null,
+    size = null,
+    uppercase = false
+  } = props;
 
   const classes = cx(["btn"], {
     "btn--light": variant === "light",
     "btn--dark": variant === "dark",
-    "btn--uppercase": uppercase
+    "btn--uppercase": uppercase,
+    "btn--sm": size === "small" || size === "sm"
   });
 
   return (

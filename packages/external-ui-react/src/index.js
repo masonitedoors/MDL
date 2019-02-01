0e0a2f02af8f86efcf4fb12475dc9c8e9c886f34
Tarik Hamilton <me@tarikhamilton.com>
2019-02-01 16:26:11 -0500

    Update exports

--- packages/external-ui-react/src/index.js
+++ packages/external-ui-react/src/index.js
@@ -1,7 +1,7 @@
-export { React as Checkbox } from "components/internal/forms/Checkbox";
-export { React as Button } from "components/internal/forms/button/Button";
-export { React as Tabs } from "components/internal/nav/Tabs";
+export { React as Checkbox } from "components/forms/Checkbox";
+export { React as Button } from "components/forms/button/Button";
+export { React as Tabs } from "components/nav/Tabs";
 export {
   ReactHeading as Heading,
   ReactP as P
-} from "components/internal/typography";
+} from "components/typography";

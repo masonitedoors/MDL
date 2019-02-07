9d307522ed784870f5cfafe50b8f359477c6eb3b
Tarik Hamilton <tarikhamilton@gmail.com>
2019-02-07 09:06:09 -0500

    Remove internal-ui-react package

--- packages/internal-ui-react/src/index.js
+++
@@ -1,8 +0,0 @@
-export { React as Checkbox } from "components/internal/forms/Checkbox";
-export { React as Button } from "components/internal/forms/button/Button";
-export {
-  ReactHeading as Heading,
-  ReactP as P
-} from "components/internal/typography";
-export { default as globalCss } from "styles/global.scss";
-export { default as styles } from "styles/index.scss";

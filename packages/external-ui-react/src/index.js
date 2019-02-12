c2997337a0478da814cf2ffc85bc0d0f904a9c75
Tarik Hamilton <me@tarikhamilton.com>
2019-02-12 09:37:25 -0500

    Add external-ui-react back

---
+++ packages/external-ui-react/src/index.js
@@ -0,0 +1,5 @@
+export { React as Checkbox } from 'components/forms/Checkbox'
+export { React as Button } from 'components/forms/button/Button'
+export { React as Input } from 'components/forms/Input'
+export { React as Tabs } from 'components/nav/Tabs'
+export { ReactHeading as Heading, ReactP as P } from 'components/typography'

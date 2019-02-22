56e0729e59089ab5dca83935222a7b64f3e2d824
Tarik Hamilton <me@tarikhamilton.com>
2019-02-22 13:38:20 -0500

    Fix Checkbox and remove stateful prop

--- components/forms/Checkbox/stories.react.js
+++ components/forms/Checkbox/stories.react.js
@@ -6,21 +6,16 @@ import Checkbox from './react'
 
 storiesOf('Forms/Checkbox', module)
   .addDecorator(withKnobs)
-  .add('checkable from parent state only', () => {
-    const checked = boolean('Checked', true)
-    return <Checkbox checked={checked} stateful={false} />
-  })
   .add(
-    'checkable with a click event',
-    withState({ checked: false })(({ store }) => (
+    'checked with a click event',
+    withState({ checked: true })(({ store }) => (
       <Checkbox
         checked={store.state.checked}
         onChange={() => store.set({ checked: !store.state.checked })}
       />
     )),
   )
-  .add('maintains state and can be updated by props', () => {
+  .add('fully controlled', () => {
     const checked = boolean('Checked', true)
     return <Checkbox checked={checked} />
   })
-  .add('checkable without props', () => <Checkbox />)

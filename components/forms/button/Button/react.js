e822d10a0fef0c8b7c9bbf78b45859a176c3603f
Joseph Fusco <jfusco@masonite.com>
2019-02-22 11:03:01 -0500

    Add PropTypes to Button
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

--- components/forms/button/Button/react.js
+++ components/forms/button/Button/react.js
@@ -1,14 +1,13 @@
 import React from 'react'
 import classNames from 'classnames/bind'
+import PropTypes from 'prop-types'
 import s from './style.module.scss'
 
 const cx = classNames.bind(s)
 
-const Button = props => {
-  const {
-    children, onClick = null, variant = null, size = null, uppercase = false,
-  } = props
-
+const Button = ({
+  children, onClick, variant, size, uppercase,
+}) => {
   const classes = cx(['btn'], {
     'btn--light': variant === 'light',
     'btn--dark': variant === 'dark',
@@ -21,6 +20,21 @@ const Button = props => {
       {children}
     </button>
   )
+}
+
+Button.propTypes = {
+  children: PropTypes.string.isRequired,
+  onClick: PropTypes.func,
+  variant: PropTypes.oneOf(['light', 'dark']),
+  size: PropTypes.oneOf(['small', 'sm']),
+  uppercase: PropTypes.bool,
+}
+
+Button.defaultProps = {
+  onClick: null,
+  variant: null,
+  size: null,
+  uppercase: false,
 }
 
 export default Button

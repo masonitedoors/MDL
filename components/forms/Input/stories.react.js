4dd3c8a90428d66a8806289dab694d6b363ff989
Joseph Fusco <jfusco@masonite.com>
2019-02-22 09:04:36 -0500

    Add missing withState
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

--- components/forms/Input/stories.react.js
+++ components/forms/Input/stories.react.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import { withState } from '@dump247/storybook-state'
 import { withKnobs, boolean, select } from '@storybook/addon-knobs'
 import Input from './react'
 

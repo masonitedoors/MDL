b0f915441d027307a94176fc02ab4d6ec0e0acc4
Joseph Fusco <jfusco@masonite.com>
2019-02-22 09:29:44 -0500

    Allow single-child for one-expression-per-line
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,10 @@
 module.exports = {
-  extends: "@masonite",
+  extends: '@masonite',
   rules: {
-    "import/prefer-default-export": 0,
-    "react/no-danger": 0,
-    "react/no-did-update-set-state": 0,
-    "no-console": 0
-  }
-};
+    'import/prefer-default-export': 0,
+    'react/no-danger': 0,
+    'react/no-did-update-set-state': 0,
+    'no-console': 0,
+    'react/jsx-one-expression-per-line': ['on', { allow: 'single-child' }],
+  },
+}

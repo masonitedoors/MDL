a3cf20ec391a57f659e50dd491d4ad5641d1c214
Joseph Fusco <jfusco@masonite.com>
2019-02-22 11:02:38 -0500

    Fix max-length issue
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,6 +5,7 @@ module.exports = {
     'react/no-danger': 0,
     'react/no-did-update-set-state': 0,
     'no-console': 0,
-    'react/jsx-one-expression-per-line': ['on', { allow: 'single-child' }],
+    'react/jsx-one-expression-per-line': ['on', { allow: 'literal' }],
+    'implicit-arrow-linebreak': 0,
   },
 }

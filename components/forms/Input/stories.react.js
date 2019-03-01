e3ccdfbcc5f8a5e6f745f38f74a98d6810562d27
Tarik Hamilton <me@tarikhamilton.com>
2019-03-01 12:54:01 -0500

    Add moving label

--- components/forms/Input/stories.react.js
+++ components/forms/Input/stories.react.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
-import { withKnobs, boolean, select } from '@storybook/addon-knobs'
+import {
+  withKnobs, boolean, select, text,
+} from '@storybook/addon-knobs'
 import { withState } from '@dump247/storybook-state'
 import Input from './react'
 
@@ -10,11 +12,14 @@ storiesOf('Forms/Input', module)
     'default',
     withState({ value: '' })(({ store }) => {
       const variant = select('Variant', { Light: 'light', Dark: 'dark' }, 'light')
+      const label = text('Label', 'Input Label')
       const error = boolean('Error State', false)
       return (
         <Input
           variant={variant}
           error={error}
+          label={label}
+          placeholder="Type something?"
           onChange={value => store.set({ value })}
           value={store.state.value}
         />

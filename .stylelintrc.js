a246f1e84253c6734f63049ac212b2dd9ec677df
Joseph Fusco <jfusco@masonite.com>
2019-03-09 22:49:34 -0500

    Update stylelint
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,11 +1,3 @@
 module.exports = {
   extends: '@masonite/stylelint-config',
-  rules: {
-    'at-rule-no-unknown': [
-      true,
-      {
-        ignoreAtRules: ['function', 'if', 'each', 'include', 'mixin'],
-      },
-    ],
-  },
 }

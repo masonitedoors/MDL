728346e3c6ec11124d7536f01d0837fe9ee04118
Justin Ashley <jdashley27@gmail.com>
2019-03-12 14:41:54 -0400

    MXP-86 removed fontSize, fixed css issues.

--- components/menu/OverflowMenu/stories.react.js
+++ components/menu/OverflowMenu/stories.react.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import { storiesOf } from '@storybook/react'
+import classNames from 'classnames/bind'
 import OverflowMenu from './react'
 import Button from '../../forms/button/Button/react'
+import storyStyles from './stories-style.module.scss'
+
+const cx = classNames.bind(storyStyles)
 
 storiesOf('Menu/Overflow Menu', module)
   .add(
@@ -11,9 +15,15 @@ storiesOf('Menu/Overflow Menu', module)
         <p>SAVED ON: 05/30/18<br />
           EDITED BY: LOREM IPSUM
         </p>
-        <Button variant="light" fullWidth uppercase>My First Button</Button>
-        <Button variant="light" fullWidth uppercase>My Second Button</Button>
-        <Button variant="light" fullWidth uppercase>My Third Button</Button>
+        <span className={cx('overflow-menu__button-wrapper')}>
+          <Button variant="light" fullWidth uppercase>My First Button</Button>
+        </span>
+        <span className={cx('overflow-menu__button-wrapper')}>
+          <Button variant="light" fullWidth uppercase>My Second Button</Button>
+        </span>
+        <span className={cx('overflow-menu__button-wrapper')}>
+          <Button variant="light" fullWidth uppercase>My Third Button</Button>
+        </span>
       </OverflowMenu>
     ),
   )

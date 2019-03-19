0be19c442fe8bdabda949a678a90f5c6e6c57b3e
MASONITE\OChhajed <ochhajed@masonite.com>
2019-03-19 11:34:29 -0400

    Review Changes

--- components/forms/RadioButton/react.test.js
+++ components/forms/RadioButton/react.test.js
@@ -30,7 +30,7 @@ describe('RadioButton', () => {
   })
 
   describe('with user actions', () => {
-    it('It should display the when clicking the radio button', () => {
+    it('should display the when clicking the radio button', () => {
       wrapper.find('input').simulate('click')
       expect(props.onChange).toHaveBeenCalledTimes(1)
     })

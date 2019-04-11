419613d34fa9d2ed5a63d5dbd5781da17f608108
Joseph Fusco <jfusco@masonite.com>
2019-04-11 11:47:15 -0400

    Remove console.log
    Signed-off-by: Joseph Fusco <jfusco@masonite.com>

--- components/forms/Toggle/react.js
+++ components/forms/Toggle/react.js
@@ -7,37 +7,34 @@ const cx = classNames.bind(s)
 
 const Toggle = ({
   checked, disabled, icon, onChange, large,
-}) => {
-  console.log(large)
-  return (
-    <div
-      aria-checked={checked}
-      className={cx(['toggle', { 'toggle--large': large }])}
-      onClick={() => !disabled && onChange()}
-      onKeyPress={() => !disabled && onChange()}
-      role="switch"
-      tabIndex={0}
-    >
-      <input
-        checked={checked}
-        className={s.toggle__input}
-        disabled={disabled}
-        readOnly
-        tabIndex="-1"
-        type="checkbox"
-      />
-      <span className={s.toggle__track} aria-hidden="true">
-        <span className={s.toggle__knob} dangerouslySetInnerHTML={{ __html: icon }} />
-        {large && (
-          <>
-            <span className={s['toggle__label-on']}>On</span>
-            <span className={s['toggle__label-off']}>Off</span>
-          </>
-        )}
-      </span>
-    </div>
-  )
-}
+}) => (
+  <div
+    aria-checked={checked}
+    className={cx(['toggle', { 'toggle--large': large }])}
+    onClick={() => !disabled && onChange()}
+    onKeyPress={() => !disabled && onChange()}
+    role="switch"
+    tabIndex={0}
+  >
+    <input
+      checked={checked}
+      className={s.toggle__input}
+      disabled={disabled}
+      readOnly
+      tabIndex="-1"
+      type="checkbox"
+    />
+    <span className={s.toggle__track} aria-hidden="true">
+      <span className={s.toggle__knob} dangerouslySetInnerHTML={{ __html: icon }} />
+      {large && (
+        <>
+          <span className={s['toggle__label-on']}>On</span>
+          <span className={s['toggle__label-off']}>Off</span>
+        </>
+      )}
+    </span>
+  </div>
+)
 
 Toggle.propTypes = {
   checked: PropTypes.bool,

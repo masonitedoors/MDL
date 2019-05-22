936078663a3170be4229dcad688bc0945b959397
Tarik Hamilton <me@tarikhamilton.com>
2019-05-22 07:32:00 -0400

    Update/fix TableBulkEditPopover for deployment

--- components/TableBulkEditPopover/react.js
+++ components/TableBulkEditPopover/react.js
@@ -6,12 +6,19 @@ import styles from './style.module.scss'
 const cx = classNames.bind(styles)
 
 const TableBulkEditPopover = ({ children, direction, cell }) => (
-  <th style = {{position: 'absolute'}} class={cx('popover-wrapper', {
-    'popover-wrapper--top': direction === 'top',
-    'popover-wrapper--left': direction === 'left',
-  })}
+  <th
+    style={{ position: 'absolute' }}
+    className={cx('popover-wrapper', {
+      'popover-wrapper--top': direction === 'top',
+      'popover-wrapper--left': direction === 'left',
+    })}
   >
-    <div  className={cx('popover', {'popover--top': direction === 'top', 'popover--left': direction === 'left'})}>
+    <div
+      className={cx('popover', {
+        'popover--top': direction === 'top',
+        'popover--left': direction === 'left',
+      })}
+    >
       {children}
     </div>
     <div className={cx('popover-wrapper__cell')}>{cell}</div>
@@ -23,5 +30,5 @@ export default TableBulkEditPopover
 TableBulkEditPopover.propTypes = {
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
   direction: PropTypes.string.isRequired,
-  cell: PropTypes.string.isRequired
+  cell: PropTypes.string.isRequired,
 }

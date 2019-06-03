c31636a74749a8e3f64eccbc165bfd809de2efa5
Tarik Hamilton <me@tarikhamilton.com>
2019-06-03 13:56:53 -0400

    Working

---
+++ components/SidebarNav/react.js
@@ -0,0 +1,173 @@
+import React, { useState, useReducer } from 'react'
+import { mChevronsLeft, mChevronsRight } from '@masonite/svg-icons'
+import classNames from 'classnames/bind'
+import PropTypes from 'prop-types'
+import styles from './style.module.scss'
+
+const cx = classNames.bind(styles)
+
+const menuReducer = (state, { type, index: selectedIndex = null }) => {
+  switch (type) {
+    case 'SHOW_MENU':
+      return state.map((showing, i) => (selectedIndex === i ? true : showing))
+    case 'HIDE_ALL_MENUS':
+      return state.map(showing => false)
+    default:
+      return state
+  }
+}
+
+const Submenu = ({ items, expanded, dispatchMenuState }) => (
+  <ul
+    onMouseOut={() => dispatchMenuState({ type: 'HIDE_ALL_MENUS' })}
+    onBlur={() => dispatchMenuState({ type: 'HIDE_ALL_MENUS' })}
+    className={cx('sidebar-nav-submenu', {
+      'sidebar-nav-submenu--expanded': expanded,
+    })}
+  >
+    {items.map(({ label, href = '#', onClick }) => (
+      <li key={label}>
+        <a
+          className={cx('menu-item-link')}
+          href={href}
+          tabIndex="0"
+          onClick={e => {
+            if (onClick) {
+              e.preventDefault()
+              onClick()
+            }
+          }}
+        >
+          {label}
+        </a>
+      </li>
+    ))}
+  </ul>
+)
+
+const MainMenuItem = ({
+  icon,
+  href,
+  label,
+  onClick,
+  items = [],
+  index,
+  menuState,
+  dispatchMenuState,
+  showMainMenuLabels,
+  showingMainMenuLabels,
+}) => {
+  const onMouseOver = () => {
+    dispatchMenuState({ type: 'HIDE_ALL_MENUS' })
+    if (items.length > 0) dispatchMenuState({ type: 'SHOW_MENU', index })
+  }
+  return (
+    <li key={label} className={cx('menu-item')} onMouseOver={onMouseOver} onFocus={onMouseOver}>
+      <a
+        tabIndex="0"
+        className={cx('menu-item-link')}
+        href={href}
+        onClick={e => {
+          if (onClick) {
+            e.preventDefault()
+            onClick()
+          }
+        }}
+      >
+        <span className={cx('menu-item-link__icon')}>{icon}</span>
+        <span
+          className={cx('menu-item-link__label', {
+            'menu-item-link__label--hide': !showingMainMenuLabels,
+            'menu-item-link__label--hidden': !showMainMenuLabels,
+          })}
+        >
+          {label}
+        </span>
+      </a>
+
+      {items.length > 0 && (
+        <Submenu items={items} expanded={menuState[index]} dispatchMenuState={dispatchMenuState} />
+      )}
+    </li>
+  )
+}
+
+const SidebarNav = ({
+  menuItems = [], bottomMenuItems = [], logo, expandedCb,
+}) => {
+  const [menuState, dispatchMenuState] = useReducer(menuReducer, menuItems.map(v => false))
+  const [showingMainMenuLabels, setShowingMainMenuLabels] = useState(false)
+  const [showMainMenuLabels, setShowMainMenuLabels] = useState(false)
+  const menuItemDependencies = {
+    menuState,
+    dispatchMenuState,
+    showMainMenuLabels,
+    showingMainMenuLabels,
+  }
+
+  expandedCb(showingMainMenuLabels)
+
+  return (
+    <aside className={cx('sidebar')}>
+      <nav
+        className={cx('main-menu', {
+          'main-menu--expanded': showingMainMenuLabels,
+        })}
+        onTransitionEnd={() => setShowMainMenuLabels(showingMainMenuLabels)}
+      >
+        <ul className={cx('main-menu__top')}>
+          <li className={cx(['menu-item', 'menu-item--logo'])}>{logo}</li>
+          {menuItems.map((props, index) => (
+            <MainMenuItem index={index} {...props} {...menuItemDependencies} />
+          ))}
+        </ul>
+        <ul className={cx('main-menu__bottom')}>
+          {bottomMenuItems.map((props, index) => (
+            <MainMenuItem index={index} {...props} {...menuItemDependencies} />
+          ))}
+        </ul>
+      </nav>
+
+      <div
+        className={cx('expand-btn')}
+        onClick={() => setShowingMainMenuLabels(!showingMainMenuLabels)}
+        onKeyDown={e => e.key === 'Enter' && setShowingMainMenuLabels(!showingMainMenuLabels)}
+        title="Press enter to toggle display of main menu item labels"
+        tabIndex={0}
+        role="button"
+      >
+        <span
+          className={cx('expand-btn__icon')}
+          dangerouslySetInnerHTML={{
+            __html: showMainMenuLabels ? mChevronsLeft : mChevronsRight,
+          }}
+        />
+      </div>
+    </aside>
+  )
+}
+
+const menuItemShape = PropTypes.arrayOf(
+  PropTypes.shape({
+    icon: PropTypes.element,
+    label: PropTypes.string,
+    href: PropTypes.string,
+    onClick: PropTypes.func,
+  }),
+)
+
+SidebarNav.propTypes = {
+  logo: PropTypes.element,
+  menuItems: menuItemShape,
+  bottomMenuItems: menuItemShape,
+  expandedCb: PropTypes.func,
+}
+
+SidebarNav.defaultProps = {
+  logo: null,
+  menuItems: [],
+  bottomMenuItems: [],
+  expandedCb: () => {},
+}
+
+export default SidebarNav
---
+++ components/SidebarNav/react.stories.js
@@ -0,0 +1,84 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { withKnobs } from '@storybook/addon-knobs'
+import { withState } from '@dump247/storybook-state'
+import {
+  mEntry, mInterior, mExport, mContact, mSignout,
+} from '@masonite/svg-icons'
+import logo from 'images/masonite-icon.png'
+import SidebarNav from './react'
+import Readme from './react.README.md'
+
+const addKey = item => ({ ...item, key: item.label })
+
+storiesOf('Sidebar', module)
+  .addParameters({ readme: { sidebar: Readme } })
+  .addDecorator(withKnobs)
+  .add('Default', () => {
+    const onClick = () => console.log('Mock onClick call')
+    const menuItems = [
+      {
+        label: 'Exterior Doors',
+        icon: <span dangerouslySetInnerHTML={{ __html: mEntry }} />,
+        href: '/',
+        items: [
+          {
+            label: 'Net Price Management',
+            href: '/',
+            onClick,
+          },
+          {
+            label: 'Glass Management',
+            href: '/glass/inventory',
+            onClick,
+          },
+          {
+            label: 'Slab Management',
+            href: '/slab/inventory',
+            onClick,
+          },
+          {
+            label: 'Factory Glazed Management',
+            href: '/factory-glazed/inventory',
+            onClick,
+          },
+        ].map(addKey),
+      },
+      {
+        label: 'Interior Doors',
+        icon: <span dangerouslySetInnerHTML={{ __html: mInterior }} />,
+        href: '/interior/',
+      },
+      {
+        label: 'Export',
+        icon: <span dangerouslySetInnerHTML={{ __html: mExport }} />,
+        href: '#',
+      },
+      {
+        label: 'Contact',
+        icon: <span dangerouslySetInnerHTML={{ __html: mContact }} />,
+        href: '#',
+      },
+    ].map(addKey)
+
+    const bottomMenuItems = [
+      {
+        label: 'Log Out',
+        icon: <span dangerouslySetInnerHTML={{ __html: mSignout }} />,
+      },
+    ].map(addKey)
+
+    return (
+      <div style={{ width: '100%', height: '100%', margin: '-8px' }}>
+        <SidebarNav
+          logo={(
+            <a href="/" tabIndex="0">
+              <img src={logo} alt="Back to Landing" />
+            </a>
+)}
+          menuItems={menuItems}
+          bottomMenuItems={bottomMenuItems}
+        />
+      </div>
+    )
+  })

7f7bc0a5e0e1ef5ee48c26d783808802b7b7618c
Tarik Hamilton <me@tarikhamilton.com>
2019-10-04 10:45:18 -0400

    Fix anchor tag having an explicit font size

--- components/typography/react.stories.js
+++ components/typography/react.stories.js
@@ -7,7 +7,7 @@ import Readme from './react.README.md'
 storiesOf('Typography', module)
   .addParameters({ readme: { sidebar: Readme } })
   .addDecorator(withKnobs)
-  .add('All', () => {
+  .add('React', () => {
     // const darkMode = boolean("Dark Mode", false);
     const headings = [...Array(6).keys()].map(level => {
       const lvl = level + 1
@@ -30,3 +30,22 @@ storiesOf('Typography', module)
       </Fragment>
     )
   })
+  .add('HTML element tags', () => (
+    <article>
+      <section>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+    incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, <a href="#">quis nostrud</a> exercitation <span>ullamco laboris nisi ut aliquip</span> ex ea commodo consequat.
+        </p>
+        <p>Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt
+    mollit anim id est laborum.
+        </p>
+      </section>
+      <section>
+        <ul>
+          <li>Unordered list item 1</li>
+          <li>Unordered list item 2</li>
+        </ul>
+      </section>
+    </article>
+  ))

dca43b170412ee2f1b3415c39abed9b85517b441
Joseph Fusco <hello@josephfus.co>
2019-10-18 16:04:13 -0400

    First pass for pagination component
    Signed-off-by: Joseph Fusco <hello@josephfus.co>

--- components/Pagination/react.js
+++ components/Pagination/react.js
@@ -1,58 +1,67 @@
 import React from 'react'
 import classNames from 'classnames/bind'
-// import { Button } from 'components/Button'
+import { React as Button } from 'components/Button'
 
 import styles from './style.module.scss'
 
 const cx = classNames.bind(styles)
 
 const Pagination = ({
-  disabled, // Disable backward/forward buttons.
-  page, // The current page.
-  total, // Total number of items.
-  pageSize, // Number of items per page.
-  pageSizes, // Choices of `pageSize`.
-  onChange,
+  currentPageNumber, totalItems, itemsPerPage = 10, onChange,
 }) => {
-  console.log('disabled', disabled)
-  console.log('page', page)
-  console.log('total', total)
-  console.log('pageSize', pageSize)
-  console.log('pageSizes', pageSizes)
-  console.log('onChange', onChange)
+  // Calculate the total number of pages.
+  const totalPages = Math.ceil(totalItems / itemsPerPage)
+
+  // Calculate the current item range for the current page.
+  const currentItemRangeMax = currentPageNumber * itemsPerPage
+  const currentItemRangeMin = currentItemRangeMax - (itemsPerPage - 1)
+  const currentItemRange = `${currentItemRangeMin}-${currentItemRangeMax} of ${totalItems}`
+
+  // Generate an array of page numbers.
+  const pageNumbers = [...Array(totalPages + 1).keys()]
+  pageNumbers.shift()
+
+  // Define our first & last page numbers.
+  const firstPageNumber = 1
+  const lastPageNumber = pageNumbers[pageNumbers.length - 1]
 
   return (
     <div className={cx('pagination')}>
       <div className={cx('pagination__info')}>
-        <span className={cx('pagination__info-viewing-label')}>Viewing</span> 1-30 of 99
+        <span className={cx('pagination__info-viewing-label')}>Viewing </span>
+        {currentItemRange}
       </div>
-      <ul className={cx('pagination__nav')}>
-        <li className={cx('pagination__nav-item')}>
-          <button className={cx('pagination__nav-link')} type="button">
-            1
-          </button>
-        </li>
-        <li className={cx(['pagination__nav-item', 'pagination__nav-item--active'])}>
-          <button className={cx('pagination__nav-link')} type="button">
-            2
-          </button>
-        </li>
-        <li className={cx('pagination__nav-item')}>
-          <button className={cx('pagination__nav-link')} type="button">
-            3
-          </button>
-        </li>
-        <li className={cx('pagination__nav-item')}>
-          <button className={cx('pagination__nav-link')} type="button">
-            4
-          </button>
-        </li>
-        <li className={cx('pagination__nav-item')}>
-          <button className={cx('pagination__nav-link')} type="button">
-            5
-          </button>
-        </li>
-      </ul>
+      <nav role="navigation" aria-label="Pagination Navigation">
+        <ul className={cx('pagination__nav')}>
+          <li className={cx(['pagination__nav-item', 'pagination__nav-item-prev'])}>
+            <Button size="small" disabled={currentPageNumber === firstPageNumber}>
+              Prev
+            </Button>
+          </li>
+          {pageNumbers.map(pageNumber => (
+            <li
+              className={cx([
+                'pagination__nav-item',
+                currentPageNumber === pageNumber && 'pagination__nav-item--active',
+              ])}
+              key={pageNumber}
+            >
+              <button
+                className={cx('pagination__nav-link')}
+                aria-label={`Goto Page ${pageNumber}`}
+                type="button"
+              >
+                {pageNumber}
+              </button>
+            </li>
+          ))}
+          <li className={cx(['pagination__nav-item', 'pagination__nav-item-next'])}>
+            <Button size="small" disabled={currentPageNumber === lastPageNumber}>
+              Next
+            </Button>
+          </li>
+        </ul>
+      </nav>
     </div>
   )
 }

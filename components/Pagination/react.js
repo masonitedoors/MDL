4d3211fc502e7d6bea32e4b0d634e3d5aaae2807
Joseph Fusco <hello@josephfus.co>
2019-10-21 12:45:38 -0400

    Clean up
    Signed-off-by: Joseph Fusco <hello@josephfus.co>

--- components/Pagination/react.js
+++ components/Pagination/react.js
@@ -1,7 +1,7 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 import classNames from 'classnames/bind'
 import { React as Button } from 'components/Button'
-import PropTypes from 'prop-types'
 
 import styles from './style.module.scss'
 

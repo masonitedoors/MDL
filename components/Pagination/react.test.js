8f57ca6c39afb21fdcfee2bae10c5eb10d60f990
Joseph Fusco <hello@josephfus.co>
2019-10-22 15:04:37 -0400

    Add additional pagination tests
    Signed-off-by: Joseph Fusco <hello@josephfus.co>

--- components/Pagination/react.test.js
+++ components/Pagination/react.test.js
@@ -9,10 +9,10 @@ describe('Pagination', () => {
 
   beforeEach(() => {
     props = {
-      page: 1,
+      page: 4,
       totalItems: 103,
       itemsPerPage: 10,
-      onChange: jest.fn,
+      onChange: jest.fn(),
     }
     wrapper = shallow(<Pagination {...props} />)
   })
@@ -22,6 +22,59 @@ describe('Pagination', () => {
   })
 
   it('Should display the correct number of visible pages based on total items and items per page', () => {
-    expect(wrapper.find('.pagination__nav-item').length).toBe(7)
+    expect(wrapper.find('.pagination__nav-item').length).toBe(10)
+  })
+
+  it('Should return the correct page number when a user clicks a specific page', () => {
+    const buttonPage3 = wrapper.find('button[aria-label="Page 3"]')
+    buttonPage3.simulate('click')
+    expect(props.onChange).toHaveBeenCalledTimes(1)
+    expect(props.onChange).toHaveBeenCalledWith(3)
+  })
+
+  describe('When the first page is active', () => {
+    let prevButton
+    let nextButton
+
+    beforeEach(() => {
+      props = {
+        ...props,
+        page: 1,
+      }
+      wrapper = shallow(<Pagination {...props} />)
+      prevButton = wrapper.find('.pagination__nav-item-prev Button')
+      nextButton = wrapper.find('.pagination__nav-item-next Button')
+    })
+
+    it('Should disable the `prev` page button', () => {
+      expect(prevButton.props().disabled).toBe(true)
+    })
+
+    it('Should enable the `next` page button', () => {
+      expect(nextButton.props().disabled).toBe(false)
+    })
+  })
+
+  describe('When the last page is active', () => {
+    let prevButton
+    let nextButton
+
+    beforeEach(() => {
+      props = {
+        ...props,
+        page: 11,
+      }
+      wrapper = shallow(<Pagination {...props} />)
+      prevButton = wrapper.find('.pagination__nav-item-prev Button')
+      nextButton = wrapper.find('.pagination__nav-item-next Button')
+    })
+
+    it('Should enable the `prev` page button', () => {
+      expect(prevButton.props().disabled).toBe(false)
+    })
+
+    it('Should disable the `next` page button', () => {
+      expect(nextButton.props().disabled).toBe(true)
+    })
   })
 })

74c9739c3f687e438823585779053968321079f2
Joseph Fusco <hello@josephfus.co>
2019-10-22 15:06:16 -0400

    Reset eslint config
    Signed-off-by: Joseph Fusco <hello@josephfus.co>

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,8 +5,6 @@ module.exports = {
     'react/no-danger': 0,
     'react/no-did-update-set-state': 0,
     'no-console': 0,
-    'react/prop-types': 0,
-    'jsx-a11y/anchor-is-valid': 0,
     'react/destructuring-assignment': ['error', 'always', { ignoreClassFields: true }],
     'react/jsx-one-expression-per-line': ['on', { allow: 'literal' }],
     'implicit-arrow-linebreak': 0,

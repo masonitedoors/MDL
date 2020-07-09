7e44c84304cbf82ad69921d4de105fc9149600fd
Tarik Hamilton <me@tarikhamilton.com>
2020-07-09 14:59:14 -0400

    Add onOutsideClick prop to ModalBackdrop

--- components/ModalBackdrop/react.js
+++ components/ModalBackdrop/react.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { CSSTransition } from 'react-transition-group'
 import classNames from 'classnames/bind'
@@ -7,9 +7,10 @@ import styles from './style.module.scss'
 const cx = classNames.bind(styles)
 
 const ModalBackdrop = ({
-  children, className, show, style,
+  children, className, onOutsideClick, show, style,
 }) => {
   const [entered, setEntered] = useState(false)
+  const backdropRef = useRef()
 
   useEffect(() => {
     // Inner transition won't fire, if outer transition's `in` is already true.
@@ -24,7 +25,12 @@ const ModalBackdrop = ({
       onEntered={() => setEntered(true)}
       onExited={() => setEntered(false)}
     >
-      <div className={cx('modal-backdrop', className)} style={style}>
+      <div
+        ref={backdropRef}
+        className={cx('modal-backdrop', show && 'modal-backdrop--active', className)}
+        style={style}
+        onClick={event => event.target === backdropRef.current && onOutsideClick()}
+      >
         <CSSTransition in={entered} timeout={250} classNames="modal-backdrop__content">
           <div className={cx('modal-backdrop__content')}>{children}</div>
         </CSSTransition>
@@ -36,9 +42,10 @@ const ModalBackdrop = ({
 export default ModalBackdrop
 
 ModalBackdrop.propTypes = {
-  show: PropTypes.bool,
   children: PropTypes.oneOfType([PropTypes.element, PropTypes.string]).isRequired,
   className: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
+  onOutsideClick: PropTypes.func,
+  show: PropTypes.bool,
   style: PropTypes.shape({
     backgroundColor: PropTypes.string,
     background: PropTypes.string,
@@ -47,6 +54,7 @@ ModalBackdrop.propTypes = {
 
 ModalBackdrop.defaultProps = {
   className: '',
+  onOutsideClick: null,
   show: true,
   style: null,
 }
--- components/ModalBackdrop/react.stories.js
+++ components/ModalBackdrop/react.stories.js
@@ -1,17 +1,21 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { storiesOf } from '@storybook/react'
 import { withKnobs, boolean } from '@storybook/addon-knobs'
+import { React as Button } from 'components/Button'
 import ModalBackdrop from './react'
 import Readme from './react.README.md'
 
 storiesOf('ModalBackdrop', module)
   .addParameters({ readme: { sidebar: Readme } })
-  .addDecorator(withKnobs)
   .add('default', () => {
-    const show = boolean('Show', true)
+    const [show, setShow] = useState(true)
+
     return (
-      <ModalBackdrop show={show}>
-        <p>Content shows in center</p>
-      </ModalBackdrop>
+      <>
+        <ModalBackdrop onOutsideClick={() => console.log('click') || setShow(false)} show={show}>
+          <div>Content shows in center</div>
+        </ModalBackdrop>
+        <Button onClick={() => setShow(true)}>Show Modal</Button>
+      </>
     )
   })

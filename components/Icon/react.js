4ff879543dc0584e6a5356a6233a915b42157186
Tarik Hamilton <me@tarikhamilton.com>
2020-07-09 12:20:41 -0400

    Add className and style prop to Icon

--- components/Icon/react.js
+++ components/Icon/react.js
@@ -6,34 +6,30 @@ import style from './style.module.scss'
 const cx = classNames.bind(style)
 
 const Icon = ({
-  className, svg, title, size,
-}) => {
-  const cssClasses = cx('icon', [className])
-  return (
-    <div
-      className={cssClasses}
-      style={{ height: size, width: size }}
-      dangerouslySetInnerHTML={{
-        __html: svg.replace(/<title>\w+<\/title>/, `<title>${title}</title>`),
-      }}
-    />
-  )
-}
+  className, title, size, style, svg,
+}) => (
+  <div
+    className={cx('icon', className)}
+    style={{ height: size, width: size, ...style }}
+    dangerouslySetInnerHTML={{
+      __html: svg.replace(/<title>\w+<\/title>/, `<title>${title}</title>`),
+    }}
+  />
+)
 
 export default Icon
 
 Icon.propTypes = {
-  className: PropTypes.oneOfType([
-    PropTypes.object,
-    PropTypes.arrayOf([PropTypes.string, PropTypes.object]),
-  ]),
-  svg: PropTypes.string.isRequired,
+  className: PropTypes.oneOfType([PropTypes.array, PropTypes.string]),
   title: PropTypes.string,
   size: PropTypes.number,
+  style: PropTypes.shape({}),
+  svg: PropTypes.string.isRequired,
 }
 
 Icon.defaultProps = {
   className: [],
-  title: '',
   size: 32,
+  style: null,
+  title: '',
 }

56fd63c8c04f433f93a7c069a91fbb56cbc460e1
Bill Knight <BKnight@Masonite.com>
2021-03-31 12:21:39 -0400

    Example of :global

--- components/Datepicker/react.js
+++ components/Datepicker/react.js
@@ -1,12 +1,15 @@
 import React from 'react'
+import classNames from 'classnames/bind'
 import DatePicker, { CalendarContainer } from 'react-datepicker'
-import './style.module.scss'
+import styles from './style.module.scss'
 import 'react-datepicker/dist/react-datepicker.css'
+
+const cx = classNames.bind(styles)
 
 const Datepicker = ({ className, ...props }) => {
   const MyContainer = ({ className, children }) => {
     return (
-      <div className='mdl-container-wrapper'>
+      <div className={cx('mdl-container-wrapper')}>
         <CalendarContainer className={className}>
           <div>{children}</div>
         </CalendarContainer>

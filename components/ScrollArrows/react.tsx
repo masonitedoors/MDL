e1e93a9e1dfac0f8a4523e46889a7c628dccc8be
Bill Knight <BKnight@Masonite.com>
2021-04-09 16:31:03 -0400

    MXP-891: Filter Buttons and ScrollLeft/ScrollRight arrows.

---
+++ components/ScrollArrows/react.tsx
@@ -0,0 +1,29 @@
+import React, { ButtonHTMLAttributes } from 'react'
+import classNames from 'classnames/bind'
+import { mChevronLeft, mChevronRight } from '@masonite/svg-icons'
+import Icon from '../Icon/react'
+import styles from './style.module.scss'
+
+const cx = classNames.bind(styles)
+
+export interface ScrollProps extends Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick'> {
+  onClick: (direction: number) => void
+}
+
+const ScrollBase = ({
+  left,
+  className,
+  ...rest
+}: { left?: boolean } & ButtonHTMLAttributes<HTMLButtonElement>) => (
+  <button className={cx(className, 'scroller')} type="button" {...rest}>
+    <Icon svg={left ? mChevronLeft : mChevronRight} title={left ? 'Left' : 'Right'} size={36} />
+  </button>
+)
+
+export const ScrollLeft = ({ onClick, ...rest }: ScrollProps) => (
+  <ScrollBase left onClick={() => onClick && onClick(-1)} {...rest} />
+)
+
+export const ScrollRight = ({ onClick, ...rest }: ScrollProps) => (
+  <ScrollBase onClick={() => onClick && onClick(1)} {...rest} />
+)

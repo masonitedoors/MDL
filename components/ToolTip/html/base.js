13fd2443eed06c760719d295206a930bac760816
Tarik Hamilton <me@tarikhamilton.com>
2021-05-14 21:09:01 -0400

    Remove html examples and stories and @storybook/html

--- components/ToolTip/html/base.js
+++
@@ -1,9 +0,0 @@
-export const changeTooltip = (element) => {
-  const classItem = 'tooltip--display-tooltip'
-
-  if (element.classList.contains(classItem)) {
-    element.classList.remove(classItem)
-  } else {
-    element.classList.add(classItem)
-  }
-}
--- components/typography/html.stories.js
+++
@@ -1,4 +0,0 @@
-import { storiesOf } from '@storybook/html'
-import html from './html.html'
-
-storiesOf('Typography/Content', module).add('default', () => html)
